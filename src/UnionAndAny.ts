a97755ede844ec8d0a125319bd2e92449b761287
lifelinecoding <apatel89329@gmail.com>
2026-07-04 09:54:47 +0530

    Topic : Union & Any Types

---
+++ src/UnionAndAny.ts
@@ -0,0 +1,17 @@
+// Union
+
+let data: number | string | boolean = 42;
+data = "Aditya Patel";
+data = true;
+
+let status: "Pending" | "Sucessful" | "Cancelled" = "Pending";
+status = "Sucessful";
+status = "Cancelled";
+
+//Any : It is recommended to avoid the any type always
+let newData;
+
+newData = "Aditya"
+newData = 78
+
+//Here newData is of any type that allows any value to be assign in the variable.
