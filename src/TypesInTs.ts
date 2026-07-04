f0050bbf7627239a1b32560d0d616815314220d5
lifelinecoding <apatel89329@gmail.com>
2026-07-04 09:21:42 +0530

    Topic: Type Annotaions & Inference

---
+++ src/TypesInTs.ts
@@ -0,0 +1,6 @@
+// Type Annotations :- Type annotaion means we have to define the type while declaration
+let name: string = "Aditya Patel";
+const rand: number = Math.random() > 0.5 ? 10 : 5;
+
+// Type Inference :- Type inference means it automatically understands which type of data will be assigned
+let user = "Aditya Patel";
